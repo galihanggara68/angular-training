b2e59963f2a1f81da466934a88563ff79290d690
galihanggara68 <galihanggara68@gmail.com>
2023-02-22 17:38:54 +0700

    2. Basic

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: 'server.component.html',
+  styles: [
+    `
+      .online {
+        color: white;
+      }
+    `,
+  ],
+})
+export class ServerComponent {
+  serverId: number = 10;
+  serverStatus: string = 'single';
+
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
+  getServerStatus() {
+    return this.serverStatus;
+  }
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
+  }
+}
